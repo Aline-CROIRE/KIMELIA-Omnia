a226d96c225cee8f467df058d3fede9f5495ec7b
Aline NIYONIZERA <niyocroirealine@gmail.com>
2026-04-01 23:58:02 +0200

    Update App.js

--- KimeliaOmniaMobile/App.js
+++ KimeliaOmniaMobile/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
-
-
 import { useFonts } from 'expo-font';
 import {
   Poppins_400Regular,
